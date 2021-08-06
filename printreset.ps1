f3575545e03a5bf3745145809a820597ec97b06d
Kyle Rode <52089154+mytech-today-now@users.noreply.github.com>
2021-08-06 14:26:37 -0500

    Reset Print spool service

--- printreset.ps1
+++ printreset.ps1
@@ -1,7 +1,2 @@
-# Reset Print Spooler for Windows Comptuers
-
-net stop spooler
-
-DEL /F /S /Q %systemroot%\System32\spool\PRINTERS\*
-
-net start spooler
+# Local computer
+Restart-Service -Name Spooler -Force
